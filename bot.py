ac8998d9bd6a82db7e6e96b3124b45f70348d55d
pi31415einstein <36282526+pi31415einstein@users.noreply.github.com>
2020-06-06 12:00:58 +1000

    Formatted comments

--- bot.py
+++ bot.py
@@ -2,16 +2,16 @@
 import os, discord
 from dotenv import load_dotenv
 
-#Get tokens from .env
+# Get tokens from .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-#Removes either the username or discord tag.
+# Removes either the username or discord tag.
 detagger = lambda a:str(a)[:-5]
 denamer = lambda a:str(a)[-5:]
 
-#Reads the contents of the user lists on startup.
+# Reads the contents of the user lists on startup.
 f = open('admins.txt', 'r')
 admins = list(map(lambda a:a.rstrip(),f))   #Iterates through all lines in the file and strips newline chars from the end.
 f.close()
@@ -30,7 +30,7 @@ async def on_ready():
 @client.event
 async def on_typing(channel, user, when):
     global bot_state
-    #Check if the user currently typing is the bot to prevent recursion
+    # Check if the user currently typing is the bot to prevent recursion
     if user == client.user:
         return
 
@@ -45,13 +45,13 @@ async def on_message(message):
     global bot_state
     print(message.content)
     message_author_tag = denamer(message.author)
-    if message_author_tag != '#3842' and bot_state:      #User #3842 is the bot.
+    if message_author_tag != '#3842' and bot_state:      # User #3842 is the bot.
         if message_author_tag in muted:
             await message.delete()
             print(f'\"{message}\" from {message.author} was deleted.', end='\n\n')
             await message.author.send('Please cease the actions of sending messages')
     elif message_author_tag in admins:
-        print(message.content, end='\n\n')
+        print(message.content, end='\n\n') # debug
         if message.content.startswith('stop'):
             bot_state = False
             print(f'Bot has been turned off by {message.author}')
@@ -60,4 +60,5 @@ async def on_message(message):
             bot_state = True
             print(f'Bot has been turned on by {message.author}')
             await message.channel.send(f'Bot has been turned on by {message.author}')
+
 client.run(TOKEN)
